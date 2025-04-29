1d59a07f198aaef525d0c6045c2cb97a616ecf96
walterbsbbr <114318256+walterbsbbr@users.noreply.github.com>
2025-04-29 19:00:36 -0300

    Update downloader.py

--- utils/downloader.py
+++ utils/downloader.py
@@ -4,9 +4,15 @@ import yt_dlp
 class DownloadError(Exception):
     pass
 
+def sanitize_filename(title):
+    """Corta o nome se for muito longo"""
+    if len(title) > 100:
+        return title[:100]
+    return title
+
 def download_video(url, output_dir):
     ydl_opts = {
-        'outtmpl': os.path.join(output_dir, '%(title)s.%(ext)s'),
+        'outtmpl': os.path.join(output_dir, '%(title).100s.%(ext)s'),  # Limita o nome a 100 caracteres
         'format': 'bestvideo+bestaudio/best',
     }
 
