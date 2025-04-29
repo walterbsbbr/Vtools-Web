ede1617ff0931ee5d7e6cba0efad0e27b04eb5df
walterbsbbr <114318256+walterbsbbr@users.noreply.github.com>
2025-04-29 18:30:26 -0300

    Update app.py
    Corrigido: app rodando em 0.0.0.0 e porta do Render

--- app.py
+++ app.py
@@ -72,4 +72,6 @@ def uploaded_file(filename):
     return send_file(os.path.join(UPLOAD_FOLDER, filename))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
